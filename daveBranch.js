8de1c42377078f2f54e2922383d8196f6f7866db
davidsherry14 <davidsherry1407@gmail.com>
2024-12-05 15:25:35 +0000

    fixing a bug from last commit

--- daveBranch.js
+++ daveBranch.js
@@ -1,6 +1,5 @@
 let userPick = "rock";
 let computerPick = "scissors";
-let playAgain = true;
 
 let playAgain = true;
 
@@ -10,17 +9,17 @@ while (playAgain) {
   let compScore = 0;
 
   if (userPick === computerPick) {
-    result = "It's a draw! Each of you chose ${userPick}.";
+    result = `It's a draw! Each of you chose ${userPick}.`;
   } else if (
     (userPick === "rock" && computerPick === "scissors") ||
     (userPick === "paper" && computerPick === "rock") ||
     (userPick === "scissors" && computerPick === "paper")
   ) {
-    result = "You win! ${userPick} beats ${computerPick}.";
+    result = `You win! ${userPick} beats ${computerPick}.`;
     userScore += 1;
     compScore -= 1;
   } else {
-    result = "You lose! ${computerPick} beats ${userPick}.";
+    result = `You lose! ${computerPick} beats ${userPick}.`;
     userScore -= 1;
     compScore += 1;
   }
@@ -31,6 +30,6 @@ while (playAgain) {
     playAgain = true;
   } else {
     playAgain = false;
-    alert(Thanks for playing, your score is ${userScore});
+    alert(`Thanks for playing, your score is ${userScore}`);
   }
 }
