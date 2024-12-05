bdcc64547fcd93fdc0bcd035555fb9e53a09e7ec
davidsherry14 <davidsherry1407@gmail.com>
2024-12-05 15:18:50 +0000

    updated work, now with while loop functionality and responses

--- daveBranch.js
+++ daveBranch.js
@@ -1,19 +1,36 @@
-let result = "";
-let score = 0;
 let userPick = "rock";
 let computerPick = "scissors";
+let playAgain = true;
 
-if (userPick === computerPick) {
-  result = `It's a draw! Each of you chose ${userPick}.`;
-} else if (
-  (userPick === "rock" && computerPick === "scissors") ||
-  (userPick === "paper" && computerPick === "rock") ||
-  (userPick === "scissors" && computerPick === "paper")
-) {
-  result = `You win! ${userPick} beats ${computerPick}.`;
-  score += 1;
-} else {
-  result = `You lose! ${computerPick} beats ${userPick}.`;
-  score -= 1;
+let playAgain = true;
+
+while (playAgain) {
+  let result;
+  let userScore = 0;
+  let compScore = 0;
+
+  if (userPick === computerPick) {
+    result = "It's a draw! Each of you chose ${userPick}.";
+  } else if (
+    (userPick === "rock" && computerPick === "scissors") ||
+    (userPick === "paper" && computerPick === "rock") ||
+    (userPick === "scissors" && computerPick === "paper")
+  ) {
+    result = "You win! ${userPick} beats ${computerPick}.";
+    userScore += 1;
+    compScore -= 1;
+  } else {
+    result = "You lose! ${computerPick} beats ${userPick}.";
+    userScore -= 1;
+    compScore += 1;
+  }
+
+  alert(result);
+
+  if (confirm("Do you want to play again?")) {
+    playAgain = true;
+  } else {
+    playAgain = false;
+    alert(Thanks for playing, your score is ${userScore});
+  }
 }
-console.log(result);
